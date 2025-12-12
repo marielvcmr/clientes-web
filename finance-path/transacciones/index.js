be84aaac9c28c8b1187999338c7ca507e5a5090d
Mariangeles Mavarez <mariangelesmavarez@gmail.com>
2025-12-12 14:07:15 -0400

    feat: table transacciones

--- finance-path/transacciones/index.js
+++ finance-path/transacciones/index.js
@@ -42,6 +42,91 @@ class dataCard {
 
 
 
+//table//
+class TablaTransacciones {
+
+    constructor(containerId) {
+        this.containerId = containerId;
+        this.container = document.getElementById(containerId);
+
+        this.container.innerHTML = "";
+
+        // contador interno de IDs
+        this.nextId = 1;
+
+        // crear tabla principal
+        this.tabla = document.createElement("div");
+        this.tabla.className = "tabla-container";
+
+        // agregar header
+        this.tabla.appendChild(this._crearHeader());
+
+        // meter tabla en el contenedor
+        this.container.appendChild(this.tabla);
+    }
+
+
+    // ---------------------------
+    // MÉTODO PARA AGREGAR FILAS
+    // ---------------------------
+    agregar(item) {
+        // asignar ID automáticamente
+        const itemConId = {
+            id: this.nextId++,
+            ...item
+        };
+
+        const fila = this._crearFila(itemConId);
+        this.tabla.appendChild(fila);
+    }
+
+
+    // ---------------------------
+    // HEADER
+    // ---------------------------
+    _crearHeader() {
+        const h = document.createElement("div");
+        h.className = "tabla-header";
+
+        h.innerHTML = `
+            <div>ID</div>
+            <div>Fecha</div>
+            <div>Descripcion</div>
+            <div>Categoria</div>
+            <div>Tipo</div>
+            <div>Monto</div>
+            <div>Opciones</div>
+        `;
+
+        return h;
+    }
+
+
+    // ---------------------------
+    // CREAR FILA
+    // ---------------------------
+
+    _crearFila(item) {
+    const row = document.createElement("div");
+    row.className = "tabla-row";
+
+    row.innerHTML = `
+        <div class="col-id">${item.id}</div>
+        <div class="col-fecha">${item.fecha}</div>
+        <div class="col-desc">${item.descripcion}</div>
+        <div class="col-cat">${item.categoria}</div>
+        <div class="col-tipo">${item.tipo}</div>
+        <div class="col-monto">${item.monto}</div>
+
+        <div class="col-acciones">
+            <span class="btn-accion btn-edit">✏️</span>
+            <span class="btn-accion btn-delete">❌</span>
+        </div>
+    `;
+        return row;
+    }
+
+}
 
 
 
@@ -100,6 +185,11 @@ const panelTrans = document.createElement("div");
 panelTrans.className = "panel-Trans";
 dashboardScreen.appendChild(panelTrans);
 
+const tablaTrans = document.createElement("div");
+tablaTrans.className = "tabla-Trans";
+dashboardScreen.appendChild(tablaTrans);
+tablaTrans.id= 'tabla-Trans';
+
 ///////////////////////data////////////////////////
 const ingresosCard = new dataCard('Ingresos', 50, '../imgs/ingreso1.png');
 ingresosCard.render(dataTrans);
@@ -124,10 +214,26 @@ searchBar.appendChild(textInput);
 
 panelTrans.appendChild(searchBar);
 
-
-
 const Agregar = new filterWithImage('Agregar', '../imgs/add.png', ()=>{window.alert("Agregar");})
 Agregar.render(panelTrans);
 
 const Filtrar = new filterWithImage('Filtrar', '../imgs/filter1.png', ()=>{window.alert("Filtrar");})
 Filtrar.render(panelTrans);
+
+//////////////////////////tabla/////////////////////////////////////////
+const tabla = new TablaTransacciones("tabla-Trans");
+tabla.agregar({
+    fecha: "12/2/25",
+    descripcion: "Arroz",
+    categoria: "Alimentacion",
+    tipo: "Ingreso",
+    monto: 20
+});
+tabla.agregar({
+    fecha: "13/2/25",
+    descripcion: "Taxi",
+    categoria: "Movilidad",
+    tipo: "Gasto",
+    monto: 8
+});
+
